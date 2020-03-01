e936315256b8ba5c7ddd70105e77654ce0eec1c6
Caitlyn <caitlynbakery@email.com>
2020-03-01 10:55:42 -0800

    finished project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import fetch from "node-fetch";
 
 function App() {
+
+  const [animalImages, setAnimalImages] = useState([]);
+
+  const key = '15419233-8d28ff99c4a3abd7ff68c5e3b';
+
+  function getImages(color, noun) {
+    fetch(`https://pixabay.com/api/?key=${key}&q=${color}+${noun}&image_type=photo`)
+    .then(res => res.json())
+    .then(json => {
+      console.log(json)
+
+      setAnimalImages(previousData => {
+        if (previousData) {
+          console.log("previous data exists");
+          return json.hits.concat(previousData);
+        } else {
+          return json.hits;
+        }
+      });
+
+    })
+  }
+ 
+
+  useEffect(() => {
+    getImages("yellow", "animal");
+    getImages("red", "rock");
+    getImages("green", "plant");
+    getImages("happy", "cat");
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1>Pix 3</h1>
+      {animalImages.map((animal, index) => 
+      <img 
+        key={index} 
+        src={animal.previewURL} 
+        alt="animal" />
+      )}
+
     </div>
   );
 }
